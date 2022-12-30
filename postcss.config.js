5ff8e841bf808c1108bd204fdfdf35f5150cdf92
Richard Lock <lock.j.h@gmail.com>
2022-12-30 13:29:43 -0500

    chore: migrate to Gatsby v5. Warn gatsby-plugin-purgecss is behind on dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,7 @@
 const tailwindcss = require(`tailwindcss`);
 const autoprefixer = require(`autoprefixer`);
 const postCssPreset = require('postcss-preset-env');
-const postCssImport = require('postcss-import');
 
 module.exports = {
-  plugins: [
-    postCssImport,
-    tailwindcss,
-    autoprefixer,
-    postCssPreset({ stage: 1 }),
-  ],
+  plugins: [tailwindcss, autoprefixer, postCssPreset({ stage: 1 })],
 };
